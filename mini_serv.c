61ed63a00fddd1b9327707657d2e92970139c9dc
Anastasia Shalaginova Zetzner <anastasia@MacBook-Pro-de-Anastasia.local>
2024-05-22 11:33:35 +0200

    valide

--- mini_serv.c
+++ mini_serv.c
@@ -4,86 +4,94 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/select.h>
+#include <strings.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <sys/select.h>
 
-void putstr(char *str, int fd)
+void error()
 {
-	write(fd, str, strlen(str));
+	write(2, "Fatal error\n", 12);
 	exit(1);
 }
 
-void send_all(char *buffer, int server_fd, int client_fd, int fd_size)
+void sendall(char *buffer, int server, int client, int size)
 {
-	for (int i = 0; i <= fd_size; i++)
-		if (i != server_fd && i != client_fd)
+	for (int i = 0; i <= size; i++)
+		if (i != client && i != server)
 			send(i, buffer, strlen(buffer), 0);
 }
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	int server_fd, client_fd, size_fd, readed, db[65535] = {0}, limit = 0;
-	struct sockaddr_in server_addr;
+	int server, client, size, readed, db[65535] = {0}, limit = 0;
+	struct sockaddr_in servaddr;
 	fd_set old_fd, new_fd;
-	char buffer[200000], buffer2[150000];
+	char buffer[450000], buffer2[400000];
 
-	if (argc != 2)
-		putstr("Wrong number of arguments\n", 2);
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1)
-		putstr("Fatal error\n", 2);
-	bzero(&server_addr, sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(2130706433);
-	server_addr.sin_port = htons(atoi(argv[1]));
-	if ((bind(server_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr))) != 0)
-		putstr("Fatal error\n", 2);
-	if (listen(server_fd, 10) != 0)
-		putstr("Fatal error\n", 2);
+	if (ac != 2)
+	{
+		write(2, "Wrong number of arguments\n", 26);
+		exit(1);
+	}
+
+	server = socket(AF_INET, SOCK_STREAM, 0);
+	if (server == -1)
+		error();
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433);
+	servaddr.sin_port = htons(atoi(av[1]));
+
+	if ((bind(server, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+		error();
+	if (listen(server, 10) != 0)
+		error();
+
 	FD_ZERO(&old_fd);
 	FD_ZERO(&new_fd);
-	FD_SET(server_fd, &new_fd);
-	size_fd = server_fd;
+	FD_SET(server, &new_fd);
+	size = server;
+
 	while (1)
 	{
 		old_fd = new_fd;
-		if (select(size_fd + 1, &old_fd, NULL, NULL, NULL) < 0)
-			putstr("Fatal error\n", 2);
-		for (int id = 0; id <= size_fd; id++)
+		if (select(size + 1, &old_fd, NULL, NULL, NULL) < 0)
+			error();
+		for (int id = 0; id <= size; id++)
 		{
 			if (FD_ISSET(id, &old_fd) == 0)
 				continue;
-			if (id == server_fd)
+			if (id == server)
 			{
-				if ((client_fd = accept(server_fd, NULL, NULL)) < 0)
-					putstr("Fatal error\n", 2);
-				if (client_fd > size_fd)
-					size_fd = client_fd;
-				db[client_fd] = limit++;
-				FD_SET(client_fd, &new_fd);
-				sprintf(buffer, "server: client %d just arrived\n", db[client_fd]);
-				sendall(buffer, server_fd, client_fd, size_fd);
+				if ((client = accept(server, NULL, NULL)) < 0)
+					error();
+				if (client > size)
+					size = client;
+				db[client] = limit++;
+				FD_SET(client, &new_fd);
+				sprintf(buffer, "server: client %d just arrived\n", db[client]);
+				sendall(buffer, server, client, size);
 			}
 			else
 			{
-				bzero(buffer, 200000);
-				bzero(buffer2, 150000);
+				bzero(buffer, 450000);
+				bzero(buffer2, 400000);
 				readed = 1;
 				while (readed == 1 && (!buffer2[0] || buffer2[strlen(buffer2) - 1] != '\n'))
 					readed = recv(id, &buffer2[strlen(buffer2)], 1, 0);
-				if (readed <= 0)
+				if (readed <= 0) // <=
 				{
 					sprintf(buffer, "server: client %d just left\n", db[id]);
-					sendall(buffer, server_fd, id, size_fd);
+					sendall(buffer, server, id, size);
 					FD_CLR(id, &new_fd);
 					close(id);
 				}
 				else
 				{
 					sprintf(buffer, "client %d: %s", db[id], buffer2);
-					sendall(buffer, server_fd, id, size_fd);
+					sendall(buffer, server, id, size);
 				}
 			}
 		}
